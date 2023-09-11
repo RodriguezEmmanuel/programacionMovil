62d60b5134b671052c35bbc871942e76fd501733
RodriguezEmmanuel <e.rodriguez3701@gmail.com>
2023-09-10 19:21:27 -0600

    Tarea 1 de progamacion movil

---
+++ Tarea 1.kt
@@ -0,0 +1,13 @@
+fun main() {
+    // Imprime el mensaje usando print
+    print("HelloUPIICSA")
+
+    // Poner otro mensaje en consola
+    print(" Esto es otro mensaje")
+
+    // Imprime otro mensaje usando println
+    println(" Esto es un mensaje con salto de línea")
+
+    // Imprime otro mensaje usando println
+    println("Esto es otro mensaje con salto de línea")
+}

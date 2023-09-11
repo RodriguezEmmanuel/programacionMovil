1906a490cf364461d4d26394fc746a5e8d22335b
RodriguezEmmanuel <137366829+RodriguezEmmanuel@users.noreply.github.com>
2023-09-10 20:05:09 -0600

    Agregando la Tarea 2

---
+++ Tarea 2.kt
@@ -0,0 +1,20 @@
+fun main() {
+    // Declarar una variable decimal
+    val decimal = 3.1415
+
+    println("Numero decimal: $decimal")
+
+    // Convertir el decimal a flotante (Float)
+    val flotante = decimal.toFloat()
+
+    println("Numero flotante: $flotante")
+
+    // Declarar una variable constante con el valor de PI
+    val PI = 3.14159265359
+
+    // Calcular el perímetro de un círculo
+    val perimetro = 2 * PI * decimal
+
+    // Imprimir el resultado usando un String Template
+    println("El perímetro del círculo es: $perimetro")
+}
